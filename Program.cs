730793c5653ea7d3a07363a78a55acc8beb61ae9
jmiko4 <jmiko4@byui.edu>
2022-09-22 10:54:02 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    static string addX(List<string> board, int spot){
+        
+    }
+}
